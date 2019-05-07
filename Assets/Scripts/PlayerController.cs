e27b24278a654b5b48cf7fd4e5f2cdd67ccb9108
Stanley Tay <saintpatricktay@gmail.com>
2019-05-07 22:40:26 +0800

    Added Dino Animator and Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     public float JumpVelocity;
     private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
 
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (Input.GetAxis("Jump") == 1 && transform.position.y < -3.7f)
         {
+            playerAnimator.SetBool("Jumping", true);
             playerRigidbody.velocity = new Vector2(0.0f, JumpVelocity);
+        }
+
+        if (playerAnimator.GetBool("Jumping") && playerRigidbody.velocity.y == 0.0f)
+        {
+            playerAnimator.SetBool("Jumping", false);
         }
     }
 }
