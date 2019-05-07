6b2638bac4e9c028bb9e142b8333c8f7295df50d
Stanley Tay <saintpatricktay@gmail.com>
2019-05-07 18:34:45 +0800

    Added Basic Game Scene and Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpVelocity;
+    private Rigidbody2D playerRigidbody;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Jump") == 1 && transform.position.y < -3.7f)
+        {
+            playerRigidbody.velocity = new Vector2(0.0f, JumpVelocity);
+        }
+    }
+}
