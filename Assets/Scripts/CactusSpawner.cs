1ba8dfbf33775590bc7ef6026ac2548266eada11
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-05-07 23:16:12 +0800

    Added Cactus prefab and spawner

---
+++ Assets/Scripts/CactusSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusSpawner : MonoBehaviour
+{
+    public float WaitTime = 5f;
+    public GameObject Cactus;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCactusAtIntervals());
+    }
+
+    private IEnumerator SpawnCactusAtIntervals()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(WaitTime);
+            Instantiate(Cactus, transform.position, Quaternion.identity);
+        }
+    }
+}
