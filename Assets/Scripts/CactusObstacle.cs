e78aa2cb5d6e5e4c2632570b103fd81e631a68ea
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-05-07 22:48:22 +0800

    Added cactus movement and despawning

---
+++ Assets/Scripts/CactusObstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusObstacle : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D cactusRigidbody;
+
+    private void Start()
+    {
+        cactusRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        cactusRigidbody.velocity = new Vector2(-1f, 0) * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "De-Spawner")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
