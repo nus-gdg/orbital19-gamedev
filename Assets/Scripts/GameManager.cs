31308e57a082a4dd6b9a6dbe966ba79a9b58f7d6
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-05-09 12:26:13 +0800

    Added HighScore and Score UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public CactusSpawner Spawner;
     public PlayerController Player;
+    public Text CurrentTime;
+    public Text BestTime;
+    public HighScore HighScoreObject;
+
+    private float time = 0;
+    private bool hasGameEnded = false;
+
+    private void Start()
+    {
+        BestTime.text = HighScoreObject.Time.ToString();
+    }
+
+    private void Update()
+    {
+        if (hasGameEnded)
+        {
+            return;
+        }
+        time += Time.deltaTime;
+        CurrentTime.text = time.ToString();
+    }
 
     public void OnPlayerHit()
     {
         CactusObstacle.Speed = 0;
         Spawner.ShouldSpawn = false;
+        hasGameEnded = true;
+        if (HighScoreObject.Time < time)
+        {
+            HighScoreObject.Time = time;
+            BestTime.text = HighScoreObject.Time.ToString();
+        }
     }
 }
---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HighScore", menuName = "ScriptableObjects/HighScore")]
+public class HighScore : ScriptableObject
+{
+    public float Time = 0;
+}
